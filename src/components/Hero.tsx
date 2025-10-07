3a408a3821ed165e2616f83ee5de3d2c411128ec
Murad Alhassen <Murad.alhassen@gmail.com>
2025-10-06 22:43:02 -0700

    Update Hero component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -33,7 +33,7 @@ const Hero = () => {
           
           {/* Title */}
           <p className="text-2xl md:text-3xl text-muted-foreground font-medium">
-            Junior studying computer science
+            Computer Science Undergraduate Student
           </p>
           
           {/* Bio */}
